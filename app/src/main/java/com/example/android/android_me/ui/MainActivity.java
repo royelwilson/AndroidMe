4fabfebf8e10c5673987d9b97ed342d60cddbc10
royelwilson <royelwilson@gmail.com>
2017-08-11 01:32:55 +0530

    Added code for multipane layout

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -3,30 +3,89 @@ package com.example.android.android_me.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.Toast;
 
 import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+import java.util.List;
 
 /**
  * Created by el on 10/8/17.
  */
 
-public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener {
+public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener,
+        View.OnClickListener{
 
     private int headImageIndex;
     private int bodyImageIndex;
     private int legImageIndex;
 
+    //tracking 2 pane window
+    private boolean mTwoPane;
+    private Button nextButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nextButton = (Button) findViewById(R.id.button);
+
+        //checking if two pane window is being displayed by the presence of id - android_me_linear_layout
+        //on a single pane view - phones - this id will not be availble when the app is created.
+        if (findViewById(R.id.android_me_linear_layout) != null){
+            mTwoPane = true;
+            //The "next" button should not be visible in the
+            nextButton.setVisibility(View.GONE);
+
+            GridView gridView = (GridView) findViewById(R.id.images_grid_view);
+            gridView.setNumColumns(2);
+
+            //populate the body part fragments
+            BodyPartFragment headPartFragment = new BodyPartFragment();
+
+            headPartFragment.setImageList(AndroidImageAssets.getHeads());
+            headPartFragment.setmImageIndex(0);
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().add(R.id.head_container, headPartFragment).commit();
+
+            BodyPartFragment bodyPartFragment = new BodyPartFragment();
+            bodyPartFragment.setImageList(AndroidImageAssets.getBodies());
+            bodyPartFragment.setmImageIndex(0);
+            fragmentManager.beginTransaction().add(R.id.body_container, bodyPartFragment).commit();
+
+            BodyPartFragment legPartFragment = new BodyPartFragment();
+            legPartFragment.setImageList(AndroidImageAssets.getLegs());
+            legPartFragment.setmImageIndex(0);
+            fragmentManager.beginTransaction().add(R.id.leg_container, legPartFragment).commit();
+
+            //Also get rid of the NextButton.
+
+
+        }else{
+            mTwoPane = false;
+            nextButton.setOnClickListener(this);
+        }
+
+
+
     }
 
-    public void onNextClicked(View v){
+
+
+
+
+    //if single pane - the onClick will handle the button clicks
+    public void onClick(View v){
         //use Bundle to pass the index values to the new Activity
         Bundle b = new Bundle();
         b.putInt("headIndex",headImageIndex);
@@ -51,18 +110,42 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
         // to identify the index position
         int positionOfImage = position % 12;
 
+        //For two pane view
+        List<Integer> part = null;
+        int containerId = 0;
+
         switch(partlist){
             case 0:
                     headImageIndex = positionOfImage;
-                break;
+                    if(mTwoPane) {
+                        part = AndroidImageAssets.getHeads();
+                        containerId = R.id.head_container;
+                     }
+                    break;
             case 1:
                     bodyImageIndex = positionOfImage;
-                break;
+                    if(mTwoPane) {
+                        part = AndroidImageAssets.getBodies();
+                        containerId = R.id.body_container;
+                    }
+                    break;
             case 2:
                     legImageIndex = positionOfImage;
+                    if(mTwoPane) {
+                        part = AndroidImageAssets.getLegs();
+                        containerId = R.id.leg_container;
+                    }
                 break;
         }
 
+        if (mTwoPane){
+            BodyPartFragment partFragment = new BodyPartFragment();
+            partFragment.setImageList(part);
+            partFragment.setmImageIndex(positionOfImage);
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(containerId,partFragment).commit();
+
+        }
 
     }
 }
