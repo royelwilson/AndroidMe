9cf3ad1e436ed9bb51774f6b921058ad17f3555b
royelwilson <royelwilson@gmail.com>
2017-08-10 19:16:04 +0530

    Added interface for communication

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.android_me.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.example.android.android_me.R;
 
@@ -10,10 +11,15 @@ import com.example.android.android_me.R;
  * Created by el on 10/8/17.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onImageClick(int position) {
+        Toast.makeText(this,"Image Position = " + position, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -1,10 +1,12 @@
 package com.example.android.android_me.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.example.android.android_me.R;
@@ -15,15 +17,33 @@ import com.example.android.android_me.data.AndroidImageAssets;
  */
 
 public class MasterListFragment extends Fragment {
+
+    OnImageClickListener mOnImageClickListener;
+
+    public interface OnImageClickListener{
+        void onImageClick(int position);
+    }
+
     // Mandatory empty constructor
     public MasterListFragment() {
     }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+         //This allows the control to be passed to the host activity -  MainActivity
+        // Letting the system know that MainActivity has implemented the interface.
+           mOnImageClickListener = (OnImageClickListener) context;
+
+    }
+
 
     // Inflates the GridView of all AndroidMe images
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         final View rootView = inflater.inflate(R.layout.fragment_master_list, container, false);
+
         // Get a reference to the GridView in the fragment_master_list xml layout file
         GridView gridView = (GridView) rootView.findViewById(R.id.images_grid_view);
 
@@ -33,6 +53,13 @@ public class MasterListFragment extends Fragment {
 
         // Set the adapter on the GridView
         gridView.setAdapter(mAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mOnImageClickListener.onImageClick(position);
+            }
+        });
         // Return the root view
         return rootView;
     }
