9f5e5be8fc250b666c7423989af611a4c3d2cde0
royelwilson <royelwilson@gmail.com>
2017-08-10 12:07:30 +0530

    3 Fragments - Head Body Leg added to MainActivity

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -16,29 +16,35 @@
 
 package com.example.android.android_me.ui;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
 
 // This activity will display a custom Android image composed of three body parts: head, body, and legs
 public class AndroidMeActivity extends AppCompatActivity {
-
-    // TODO (1) Create a layout file that displays one body part image named fragment_body_part.xml
-        // This layout should contain a single ImageView
-
-    // TODO (2) Create a new class called BodyPartFragment to display an image of an Android-Me body part
-        // In this class, you'll need to implement an empty constructor and the onCreateView method
-        // TODO (3) Show the first image in the list of head images
-            // Soon, you'll update this image display code to show any image you want
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_me);
 
-        // TODO (5) Create a new BodyPartFragment instance and display it using the FragmentManager
+        BodyPartFragment headPartFragment = new BodyPartFragment();
+        headPartFragment.setImageList(AndroidImageAssets.getHeads());
+        headPartFragment.setmImageIndex(1);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().add(R.id.head_container,headPartFragment).commit();
+
+        BodyPartFragment bodyPartFragment = new BodyPartFragment();
+        bodyPartFragment.setImageList(AndroidImageAssets.getBodies());
+        bodyPartFragment.setmImageIndex(1);
+        fragmentManager.beginTransaction().add(R.id.body_container,bodyPartFragment).commit();
+
+        BodyPartFragment legPartFragment = new BodyPartFragment();
+        legPartFragment.setImageList(AndroidImageAssets.getLegs());
+        legPartFragment.setmImageIndex(1);
+        fragmentManager.beginTransaction().add(R.id.leg_container,legPartFragment).commit();
     }
 }
---
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -0,0 +1,47 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.android.android_me.R;
+
+import java.util.List;
+
+/**
+ * Created by el on 10/8/17.
+ */
+
+public class BodyPartFragment extends Fragment {
+    private List<Integer> mImageIdList;
+    private int mImageIndex;
+
+
+    //mandatory empty constructor for instantiating the fragment
+    public BodyPartFragment() {
+    }
+
+    //Inflate the fragment's layout -fragment_body_part.xml and set the image view
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_body_part, container, false);
+        //get the reference of the ImageView in the layout
+        ImageView imageView = (ImageView) view.findViewById(R.id.body_part_image_view);
+        //set the image inside the ImageView.For the time being we want to show the first image in the List<Integer> heads.
+        //imageView.setImageResource(AndroidImageAssets.getHeads().get(0));
+        imageView.setImageResource(mImageIdList.get(mImageIndex));
+        return view;
+    }
+
+    //setter methods
+    public void setImageList(List<Integer> imageList){
+        mImageIdList = imageList;
+    }
+    public void setmImageIndex(int imageIndex){
+        mImageIndex = imageIndex;
+    }
+}
